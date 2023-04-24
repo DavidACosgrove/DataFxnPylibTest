fddc75851e80c9cf283e9ab7b42a0157165d6e1c
Me <david@cozchemix.co.uk>
2023-04-24 11:37:45 +0100

    Add to tests for SAScore.

--- test_chem/test_sascore.py
+++ test_chem/test_sascore.py
@@ -1,8 +1,12 @@
-from pathlib import Path
+from pathlib import Path, WindowsPath
 from typing import Callable
 from unittest import TestCase, main
 
+from rdkit import Chem
+
 from df.data_transfer import DataFunctionRequest, DataFunctionResponse
+from df.sascorer import readFragmentScores, calculateScore
+from ruse.rdkit.rdkit_utils import standardize_mol
 from yaml_to_script import extract_script
 
 PRINTED_GUFF = False
@@ -71,9 +75,32 @@ class ScriptTest(TestCase):
         response = run_script(file_in, self._execute)
         scores = response.outputColumns[0].values
         self.assertEqual(15, len(scores))
-        self.assertAlmostEqual(3.4, scores[0], 1)
-        self.assertAlmostEqual(3.75, scores[-1], 1)
+        self.assertAlmostEqual(1.0, scores[0], 1)
+        self.assertAlmostEqual(1.2, scores[-3], 1)
         self.assertTrue(response)
+
+    def test_ertl_scores(self) -> None:
+        ertl_file = Path(__file__).parent / 'resources' / 'ertl_test_structs.smi'
+        suppl = Chem.SmilesMolSupplier(str(ertl_file), titleLine=False)
+        exp_scores = {"Mol_1": 1.0000, "Mol_2": 2.8849, "Mol_3": 1.0000,
+                      "Mol_4": 1.0874, "Mol_5": 1.0000, "Mol_6": 4.3597,
+                      "Mol_7": 3.0356, "Mol_8": 1.0000, "Mol_9": 3.7411,
+                      "Mol_10": 1.8176, "Mol_11": 4.4665, "Mol_12": 1.0000,
+                      "Mol_13": 1.0000, "Mol_14": 1.0000, "Mol_15": 4.5446,
+                      "Mol_16": 2.4960, "Mol_17": 3.8409, "Mol_18": 4.7418,
+                      "Mol_19": 1.0000, "Mol_20": 1.2344, "Mol_21": 1.0000,
+                      "Mol_22": 1.8705, "Mol_23": 1.0000, "Mol_24": 5.0739,
+                      "Mol_25": 1.0000, "Mol_26": 1.0000, "Mol_27": 4.1296,
+                      "Mol_28": 4.6406, "Mol_29": 2.2408, "Mol_30": 4.7943,
+                      "Mol_31": 1.0000, "Mol_32": 3.1092, "Mol_33": 4.7043,
+                      "Mol_34": 1.0000, "Mol_35": 1.1181, "Mol_36": 1.0000,
+                      "Mol_37": 1.0000, "Mol_38": 2.1356, "Mol_39": 3.9957,
+                      "Mol_40": 1.0000, }
+        for mol in suppl:
+            sm = standardize_mol(mol)
+            score = calculateScore(sm)
+            self.assertAlmostEqual(exp_scores[mol.GetProp("_Name")], score, 3)
+            # print(f'"{mol.GetProp("_Name")}" : {score:.4f},')
 
 
 if __name__ == '__main__':
